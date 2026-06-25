76b8b3429ad624625b7d5cd949a6ab73d80389af
343dev <343dev@users.noreply.github.com>
2026-06-25 16:44:35 +0700

    Move environment and binary validation to runtime in index.js

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
+import fs from 'node:fs';
 import path from 'node:path';
 import { arch, platform } from 'node:process';
 import { fileURLToPath } from 'node:url';
+
+const supported = {
+	arch: ['arm64', 'x64'],
+	platform: ['darwin', 'linux', 'win32'],
+};
+
+if (!supported.arch.includes(arch)) {
+	throw new Error(`"${arch}" CPU architecture is not supported`);
+}
+
+if (!supported.platform.includes(platform)) {
+	throw new Error(`"${platform}" OS platform is not supported`);
+}
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
 
 const binaryName = `gifsicle_${arch}${platform === 'win32' ? '.exe' : ''}`;
 const binaryPath = path.join(dirname, 'vendor', platform, binaryName);
+
+try {
+	await fs.promises.access(binaryPath);
+} catch {
+	throw new Error(`Could not find the gifsicle binary at ${binaryPath}. Your platform/architecture combination may not be supported.`);
+}
 
 export default binaryPath;
